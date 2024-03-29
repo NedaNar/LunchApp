ad65dad6a9c1f2a0f1d433dce776e488f892a8af
Simonas Danielius <s.danielius8@gmail.com>
2024-03-29 10:01:27 +0200

    Icon button scss styles file created

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import styles from './App.module.scss';
+import './styles/index.scss';
+import './styles/iconButtons.scss';
 
 export function App() {
   return (
     <div className={styles.container}>
       <h1>Welcome to Sourcery for FrontEnd 2024</h1>
       <p>You can delete all this HTML code and start working on your Lunch App</p>
-      <button type="submit" className={styles.accentButtonMDisabled}>
-        +
-      </button>
-      <button type="submit" className={styles.accentButtonS}>
-        +
-      </button>
-      <button type="submit" className={styles.outlinedButtonM}>
-        +
-      </button>
-      <button type="submit" className={styles.outlinedButtonS}>
-        +
-      </button>
-      <button type="submit" className={styles.tertiaryButtonM}>
-        +
-      </button>
-      <button type="submit" className={styles.tertiaryButtonS}>
-        +
-      </button>
     </div>
   );
 }
