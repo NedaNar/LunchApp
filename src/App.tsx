ba0896c826b8a44fcfddcd85e493f4b93869de48
AstaVil <aastaa@gmail.com>
2024-04-02 07:44:04 +0300

    navigation with two states - expanded and collapsed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ export function App() {
   return (
     <div className={styles.container}>
       <Navigation />
-      <h1>Navigation</h1>
       <h1>Welcome to Sourcery for FrontEnd 2024</h1>
       <p>You can delete all this HTML code and start working on your Lunch App</p>
     </div>
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,15 +1,27 @@
-// import React from 'react';
+import { useState } from 'react';
 import styles from './navigation.module.scss';
 import NavigationItem from './NavigationItem';
 import logo_horizontal from '../../assets/static/logo/logo_horizontal.svg';
-// import logo_vertical from '../../assets/static/logo/logo_vertical.svg';
+import logo_vertical from '../../assets/static/logo/logo_vertical.svg';
 
 export default function Navigation() {
+  const [collapsed, setCollapsed] = useState(true);
   return (
-    <aside className={styles.sidebar}>
+    <aside className={`${styles.sidebar} ${collapsed ? styles['sidebar--collapsed'] : ''}`}>
+      <button
+        type="button"
+        className={styles.sidebar__toggle}
+        onClick={() => setCollapsed((left) => !left)}>
+        {`${collapsed ? 'to right' : 'to left'}`}
+      </button>
+
       <nav className={styles.nav}>
         <div className={styles.nav__logo}>
-          <img src={logo_horizontal} width={77} alt="logo_horizontal" />
+          <img
+            src={collapsed ? logo_vertical : logo_horizontal}
+            alt="Logo"
+            className={styles.logo}
+          />
         </div>
         <ul className={styles.nav__list}>
           <li className={styles.nav__item}>
