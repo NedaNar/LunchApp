6ee485c6cfd61a6f483d4a3676354838f3e37e6b
Simonas Danielius <s.danielius8@gmail.com>
2024-04-08 16:25:34 +0300

    Food card creation

---
+++ src/components/FoodCard/FoodCard.tsx
@@ -0,0 +1,57 @@
+import styles from './foodCard.module.scss';
+import { Button, ButtonAppearance, ButtonSize, ButtonIcon } from '../RegularButton/Button';
+import SolarStarIcon from '../../assets/static/icons/icon_solar-star.svg?react';
+import RamenIcon from '../../assets/static/food-images/food-image_ramen.svg?react';
+
+function FoodCard() {
+  return (
+    <article className={styles.foodCard}>
+      <header className={styles.cardHeader}>
+        <figure className={styles.cardHeaderLogo}>
+          <RamenIcon />
+        </figure>
+        <div className={styles.cardSubHeader}>
+          <p className={styles.titleXS}>SKOMI</p>
+          <p className={styles.titleS}>Pho Rice Noodle Soup with Tofu</p>
+        </div>
+      </header>
+
+      <section className={styles.cardBody}>
+        <p className={styles.bodyM}>
+          Rice udon noodles in shiitake stock, fried shredded tofu. Garnished with chilli, coriander
+          and spring onions.
+        </p>
+        <div className={styles.cardSubBody}>
+          <div className={styles.evaluation}>
+            <figure>
+              <SolarStarIcon />
+            </figure>
+            <p className={styles.labelXS}>5.0</p>
+          </div>
+          <Button
+            text="More Info"
+            appearance={ButtonAppearance.TERTIARY}
+            size={ButtonSize.SMALL}
+            icon={ButtonIcon.ARROW}
+            onClick={() => {}}
+          />
+        </div>
+      </section>
+      <footer className={styles.cardFooter}>
+        <div className={styles.cardFooterText}>
+          <p className={styles.bodyS}>Price</p>
+          <p className={styles.titleL}>€5,65</p>
+        </div>
+        <Button
+          text="Add to cart"
+          appearance={ButtonAppearance.SECONDARY}
+          size={ButtonSize.SMALL}
+          icon={ButtonIcon.ADD}
+          onClick={() => {}}
+        />
+      </footer>
+    </article>
+  );
+}
+
+export default FoodCard;
