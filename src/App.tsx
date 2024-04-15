68b48993c5c7e819df42e2f5b0b666b55e1c024a
Simonas Danielius <s.danielius8@gmail.com>
2024-04-15 21:16:30 +0300

    Placing FoodCard in food-menu page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import styles from './App.module.scss';
-import FoodCard from './components/FoodCard/FoodCard';
-import { DishType } from './components/FoodCard/helpers';
 import './styles/index.scss';
 import Navigation from './components/navigation/Navigation';
 
@@ -10,19 +8,6 @@ export function App() {
     <div className={styles.container}>
       <Navigation />
       <Outlet />
-      <hr />
-      <br />
-      <br />
-      <FoodCard
-        vendor="Tasty Bites"
-        title="Pho Rice Noodle Soup with Tofu"
-        description="Rice udon noodles in  shiitake stock, fried shredded tofu. Garnish..."
-        price={5.65}
-        picture={DishType.Bowl}
-        isVegetarian
-        isSpicy
-        rating={5.0}
-      />
     </div>
   );
 }
--- src/pages/FoodMenuPage.tsx
+++ src/pages/FoodMenuPage.tsx
@@ -1,3 +1,20 @@
+import FoodCard from '../components/FoodCard/FoodCard';
+import { DishType } from '../components/FoodCard/helpers';
+
 export default function FoodMenuPage() {
-  return <h1>Food Menu page</h1>;
+  return (
+    <>
+      <h1>Food Menu page</h1>
+      <FoodCard
+        vendor="Tasty Bites"
+        title="Pho Rice Noodle Soup with Tofu"
+        description="Rice udon noodles in  shiitake stock, fried shredded tofu. Garnish..."
+        price={5.65}
+        picture={DishType.Bowl}
+        isVegetarian
+        isSpicy
+        rating={5.0}
+      />
+    </>
+  );
 }
