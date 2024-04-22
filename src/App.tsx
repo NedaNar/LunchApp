d0e795745ecdcf446acb6a4b0ed980d2548c6495
deividaskanis <deividaskan@gmail.com>
2024-04-22 15:21:22 +0300

    innitial styling and structure - no form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { Outlet } from 'react-router-dom';
 import styles from './App.module.scss';
 import './styles/index.scss';
-import UserProfile from './components/UserProfile/UserProfile';
-import Navigation from './components/navigation/Navigation';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 export function App() {
   return (
     <div className={styles.container}>
-      <UserProfile />
-      <Navigation />
-      <Outlet />
+      <LoginPage />
     </div>
   );
 }
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,11 @@
+import loginImage from '../../assets/static/login-screen-images/login-screen-image_light-orange-background.png';
+import styles from './loginPage.module.scss';
+
 export default function LoginPage() {
-  return <p>Login Page</p>;
+  return (
+    <div className={styles.layout}>
+      <img src={loginImage} alt="two people thinking about food" />
+      <div className={styles.loginForm} />
+    </div>
+  );
 }
