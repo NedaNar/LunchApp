2099ed4d08a18aec35da3e4748a84988d985cc8b
AstaVil <aastaa@gmail.com>
2024-04-24 08:12:07 +0300

    removed input from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import styles from './App.module.scss';
 import './styles/index.scss';
-import UserProfile from './components/UserProfile/UserProfile';
-// import Navigation from './components/navigation/Navigation';
-import { Input } from './components/Input/Input';
-import Textarea from './components/Input/Textarea';
+// import UserProfile from './components/UserProfile/UserProfile';
+import Navigation from './components/navigation/Navigation';
 
 export function App() {
   return (
     <div className={styles.container}>
-      <UserProfile />
-      {/* <Navigation /> */}
+      {/* <UserProfile /> */}
+      <Navigation />
       <Outlet />
-      <div>
-        <Input label="Srting for label" name="inputName" />
-        <Input label="Error" name="error" error />
-        <Input label="Disabled" name="disabled" placeholder="input disabled" disabled />
-        <Input label="Search" name="serchInput" icon />
-        <Textarea label="Textarea" name="textArea" />
-      </div>
     </div>
   );
 }
