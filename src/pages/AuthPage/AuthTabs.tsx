e6c22743752e358f7911df2a17504390532d2f8d
Simonas Danielius <s.danielius8@gmail.com>
2024-04-26 12:03:14 +0300

    PR changes

--- src/pages/AuthPage/AuthTabs.tsx
+++ src/pages/AuthPage/AuthTabs.tsx
@@ -1,33 +1,29 @@
-/* eslint-disable react/no-unused-prop-types */
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Tabs from '../../components/Tabs/Tabs';
 
 interface AuthTabsProps {
-  onTabChange?: (tab: string) => void;
   preselectedTab: number;
 }
 
-function AuthTabs({ onTabChange = () => {} }: AuthTabsProps) {
+function AuthTabs({ preselectedTab }: AuthTabsProps) {
   const authenticationTabs: { label: string; link: string }[] = [
     { label: 'Login', link: '/login' },
     { label: 'Register', link: '/register' },
   ];
 
   const location = useLocation();
-  const [selectedTab, setSelectedTab] = useState<number>(0);
+  const [selectedTab, setSelectedTab] = useState<number>(preselectedTab);
 
   useEffect(() => {
     const currentTab = authenticationTabs.findIndex((tab) => location.pathname === tab.link);
     if (currentTab !== -1) {
       setSelectedTab(currentTab);
-      onTabChange(authenticationTabs[currentTab].label);
     }
-  }, [location.pathname, authenticationTabs, onTabChange]);
+  }, [location.pathname, authenticationTabs]);
 
   const handleTabChange = (index: number) => {
     setSelectedTab(index);
-    onTabChange(authenticationTabs[index].label);
   };
 
   return (
