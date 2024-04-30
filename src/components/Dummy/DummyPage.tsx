23bec6f4eebfc88328b7d2ad813bae3763b21daf
Neda <neda.narmontaite@ktu.edu>
2024-04-30 15:32:20 +0300

    notifications

--- src/components/Dummy/DummyPage.tsx
+++ src/components/Dummy/DummyPage.tsx
@@ -1,8 +1,19 @@
+import { useRef } from 'react';
 import styles from './dummyPage.module.scss';
 import Checkmark from '../Checkmark/Checkmark';
+import ToastNotification, {
+  ToastRefObject,
+} from '../Notifications/ToastNotification/ToastNotification';
+import { NotificationType } from '../../utils/notificationUtils';
+import { Button, ButtonAppearance, ButtonSize } from '../RegularButton/Button';
 // import Dialog, { DialogIcon } from '../Dialog/Dialog';
 
 function DummyPage() {
+  const toastRef = useRef<ToastRefObject>(null);
+  const showNotification = (type: NotificationType) => {
+    toastRef.current!.showToast('This is a notification example', type);
+  };
+
   return (
     <div className={styles.dummyContainer}>
       <h1>Sprint No. 3</h1>
@@ -54,6 +65,22 @@ function DummyPage() {
                     secondaryButton
                     isCloseButtonVisible
                 /> */}
+      </div>
+      <div>
+        <h2>Notifications</h2>
+        <Button
+          text="Error notification"
+          appearance={ButtonAppearance.PRIMARY}
+          size={ButtonSize.LARGE}
+          onClick={() => showNotification(NotificationType.WARNING)}
+        />
+        <Button
+          text="Success notification"
+          appearance={ButtonAppearance.PRIMARY}
+          size={ButtonSize.LARGE}
+          onClick={() => showNotification(NotificationType.SUCCESS)}
+        />
+        <ToastNotification toastRef={toastRef} />
       </div>
     </div>
   );
