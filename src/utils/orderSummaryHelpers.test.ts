96970e20d048dca4d264942d17771b8b1a0163e4
AstaVil <aastaa@gmail.com>
2024-05-10 20:26:05 +0300

    few test case added

--- src/utils/orderSummaryHelpers.test.ts
+++ src/utils/orderSummaryHelpers.test.ts
@@ -15,24 +15,61 @@ enum DishType {
   Wrap = 'wrap',
 }
 
-const mockCartData: CartItem[] = [
-  { selectedDay: 'Monday', meal: { title: 'test', dishType: DishType.Bowl, price: 10 } },
-  { selectedDay: 'Monday', meal: { title: 'test2', dishType: DishType.Burger, price: 12 } },
+const mockCart1Data: CartItem[] = [
+  { selectedDay: 'Monday', meal: { title: 'test1', dishType: DishType.Bowl, price: 5 } },
+  { selectedDay: 'Monday', meal: { title: 'test2', dishType: DishType.Burger, price: 10 } },
+  { selectedDay: 'Friday', meal: { title: 'test3', dishType: DishType.Pizza, price: 13.556 } },
 ];
 
 describe('groupMealByDay', () => {
   it('should group cart items into meal items where key is weekday', () => {
-    expect(groupMealByDay(mockCartData)).toMatchObject({
+    expect(groupMealByDay(mockCart1Data)).toMatchObject({
       Monday: [
-        { dishType: DishType.Bowl, price: 10, title: 'test' },
-        { title: 'test2', dishType: DishType.Burger, price: 12 },
+        { dishType: DishType.Bowl, price: 5, title: 'test1' },
+        { title: 'test2', dishType: DishType.Burger, price: 10 },
       ] as MealItem[],
+      Friday: [{ title: 'test3', dishType: DishType.Pizza, price: 13.556 }] as MealItem[],
     });
   });
 });
 
 describe('calculateAndFormatTotalCartPrice', () => {
   it('should sum all prices of cart items and format price output number', () => {
-    expect(calculateAndFormatTotalCartPrice(mockCartData)).toEqual('22.00');
+    expect(calculateAndFormatTotalCartPrice(mockCart1Data)).toEqual('28.56');
+  });
+});
+
+describe('calculateAndFormatTotalCartPrice', () => {
+  const mockCartData: CartItem[] = [
+    { selectedDay: 'Monday', meal: { title: 'test6', dishType: DishType.Burger, price: 5.554 } },
+    { selectedDay: 'Friday', meal: { title: 'test7', dishType: DishType.Pizza, price: 0 } },
+  ];
+
+  it('should sum all prices of cart items and format price output number', () => {
+    expect(calculateAndFormatTotalCartPrice(mockCartData)).toEqual('5.55');
+  });
+});
+
+describe('groupMealByDay', () => {
+  const mockCartData: CartItem[] = [
+    { selectedDay: 'Monday', meal: { title: 'test5', dishType: DishType.Bowl, price: 5 } },
+    { selectedDay: 'Sunday', meal: { title: 'test5', dishType: DishType.Pasta, price: 5 } },
+  ];
+
+  it('if selectedDay dosent match data do not push data into array', () => {
+    expect(groupMealByDay(mockCartData)).toMatchObject({
+      Monday: [{ dishType: DishType.Bowl, price: 5, title: 'test5' }] as MealItem[],
+    });
+  });
+});
+
+describe('groupMealByDay', () => {
+  const mockCartData: CartItem[] = [
+    { selectedDay: '', meal: { title: 'test5', dishType: DishType.Bowl, price: 5 } },
+    { selectedDay: '', meal: { title: 'test5', dishType: DishType.Pasta, price: 5 } },
+  ];
+
+  it('if selectedDay is undefined do not push data into array', () => {
+    expect(groupMealByDay(mockCartData)).toMatchObject({});
   });
 });
--- src/utils/orderSummaryHelpers.ts
+++ src/utils/orderSummaryHelpers.ts
@@ -15,41 +15,6 @@ export function groupMealByDay(items: CartItem[]) {
     return acc;
   }, {} as ReduceAccumulator);
 }
-
 export function calculateAndFormatTotalCartPrice(items: CartItem[]) {
   return items.reduce((sum, item) => item.meal.price + sum, 0).toFixed(2);
 }
-
-// -------------------------------------------------------------------------
-// ____________________________example______________________________________
-// ____________generic function for "groupMealByDay function"_______________
-//
-
-// interface Item<T = any> {
-//   [key: string]: T;
-// }
-
-// interface MapItems<T> {
-//   [key: string]: Item<T>[];
-// }
-
-// export function mapItemsToSomethings<T extends Item>(
-//   array: T[],
-//   key: keyof T,
-//   targetKey: keyof T
-// ): MapItems<T> {
-//   return array.reduce((acc: MapItems<T>, item) => {
-//     const itemKey = String(item[key]);
-//     if (acc[itemKey] === undefined) {
-//       acc[itemKey] = [];
-//     }
-//     acc[itemKey].push(item[targetKey]);
-
-//     return acc;
-//   }, {});
-// }
-
-// generic function call example
-// const mappedMealsByDay = mapItemsToSomethings(items, 'selectedDay', 'meal');
-
-// -------------------------------------------------------------------------------------
