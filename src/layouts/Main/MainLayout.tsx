277c1383c8cbd7f78b712e81b338263ebdfdf044
Simonas Danielius <s.danielius8@gmail.com>
2024-05-12 13:20:39 +0300

    Fixing navigation imports for MainLayout and NotFoundPage

--- src/layouts/Main/MainLayout.tsx
+++ src/layouts/Main/MainLayout.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './mainLayout.module.scss';
 import '../../styles/index.scss';
-import Navigation from '../../components/Navigation/Navigation';
+import Navigation from '../../components/navigation/Navigation';
 import UserProfile from '../../components/UserProfile/UserProfile';
 import LogoHorizontal from '../../assets/static/logo/logo_horizontal.svg?react';
 import AccountIcon from '../../assets/static/icons/icon_account.svg?react';
