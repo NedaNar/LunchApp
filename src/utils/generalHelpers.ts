cdc16c2299b0852235bf82d353b887d7d30614bd
AstaVil <aastaa@gmail.com>
2024-05-20 20:49:24 +0300

    fixed dinamic location

--- src/utils/generalHelpers.ts
+++ src/utils/generalHelpers.ts
@@ -1,9 +1,6 @@
 export function formatCurrency(number: number) {
-  return new Intl.NumberFormat('en-DE', {
+  return new Intl.NumberFormat(navigator.languages[0] ?? navigator.language, {
     style: 'currency',
     currency: 'EUR',
   }).format(number);
 }
-
-// Šis komentaras bus ištrintas prieš sumerginant į developerį
-// standarinio Intl.NumberFormat  'de-DE'outputas (55,55 €) ,o  'en-US' outputas (€55.55)  nzn kaip formatuot, nebent su .replace('.', ',')  ;
